51b938332a259999ddf010720a63b27974b7dc98
nlane <nlane@udel.edu>
2020-11-12 17:30:27 -0500

    Review: Adds code review notes

--- server/api/allProducts.js
+++ server/api/allProducts.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const {Product} = require('../db/models')
 
+// What is the difference between AllProducts vs. SingleProduct routers?
+
 router.get('/', async (req, res, next) => {
   try {
     const allProducts = await Product.findAll()
@@ -9,6 +11,8 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+// who should we restrict this route to? should any user be able to create new products?
 
 //route to add products
 router.post('/', async (req, res, next) => {
