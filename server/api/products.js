d99ef43316d0911ab399dde342b7584b14f49f6a
Amanda Barrafato <amb1047@nyu.edu>
2020-11-16 13:51:53 -0500

    tested and updated userAdmin routes

--- server/api/products.js
+++ server/api/products.js
@@ -24,20 +24,20 @@ router.get('/:id', async (req, res, next) => {
 //edit product route
 router.put('/:productId', async (req, res, next) => {
   try {
-    if (!req.user.dataValues.role === 'Admin') {
+    if (req.user === undefined || req.user.role !== 'Admin') {
       res.sendStatus(403)
+    } else {
+      const updatedProductPromise = await Product.update(req.body, {
+        returning: true,
+        where: {
+          id: req.params.productId
+        }
+      })
+
+      const [numOfAffectedRows, [updatedProduct]] = updatedProductPromise
+
+      res.status(200).json(updatedProduct)
     }
-
-    const updatedProductPromise = await Product.update(req.body, {
-      returning: true,
-      where: {
-        id: req.params.productId
-      }
-    })
-
-    const [numOfAffectedRows, [updatedProduct]] = updatedProductPromise
-
-    res.status(200).json(updatedProduct)
   } catch (err) {
     next(err)
   }
@@ -46,16 +46,16 @@ router.put('/:productId', async (req, res, next) => {
 //route to delete product
 router.delete('/:productId', async (req, res, next) => {
   try {
-    if (!req.user.dataValues.role === 'Admin') {
+    if (req.user === undefined || req.user.role !== 'Admin') {
       res.sendStatus(403)
+    } else {
+      await Product.destroy({
+        where: {
+          id: req.params.productId
+        }
+      })
+      res.status(204)
     }
-
-    await Product.destroy({
-      where: {
-        id: req.params.productId
-      }
-    })
-    res.status(204)
   } catch (err) {
     next(err)
   }
@@ -64,11 +64,12 @@ router.delete('/:productId', async (req, res, next) => {
 //route to add products
 router.post('/', async (req, res, next) => {
   try {
-    if (!req.user.dataValues.role === 'Admin') {
+    if (req.user === undefined || req.user.role !== 'Admin') {
       res.sendStatus(403)
+    } else {
+      const newProduct = await Product.create(req.body)
+      res.json(newProduct)
     }
-    const newProduct = await Product.create(req.body)
-    res.json(newProduct)
   } catch (err) {
     next(err)
   }
