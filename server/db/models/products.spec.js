cdfef30366b62ea8c172494756846390d77061a2
Amanda Barrafato <amb1047@nyu.edu>
2020-11-16 17:41:40 -0500

    tests almost

--- server/db/models/products.spec.js
+++ server/db/models/products.spec.js
@@ -3,9 +3,16 @@ const db = require('../index')
 const Product = db.model('product')
 
 describe('Product model', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
+  beforeEach(async () => {
+    await db.sync({force: true})
+    await Product.create({
+      type: 'Cropped Tops',
+      name: 'Cropped Shirt',
+      price: 100,
+      quantity: 10
+    })
   })
+
   it('requires a name', async () => {
     try {
       await Product.create({
@@ -13,14 +20,20 @@ describe('Product model', () => {
       })
       throw Error('Creating this product should have failed')
     } catch (error) {
-      // console.log(error.message)
       expect(error.message).to.contain('product.name cannot be null')
     }
   })
+
   it('has a default image', async () => {
-    const corn = await Product.create({name: 'corn', type: 'Crops'})
+    const corn = await Product.create({
+      name: 'corn',
+      type: 'Crops',
+      quantity: 10,
+      price: 1000
+    })
     expect(corn.imageUrl).to.be.equal('/img/cornPic.jpg')
   })
+
   it('name cannot be empty', async () => {
     try {
       await Product.create({type: 'Crops', name: ''})
@@ -29,5 +42,90 @@ describe('Product model', () => {
       // console.log(error.message)
       expect(error.message).to.contain('Validation notEmpty on name failed')
     }
+  })
+
+  it('quantity cannot be null', async () => {
+    try {
+      await Product.create({type: 'Crops', name: 'Crops'})
+      throw Error('Creating this product should have failed')
+    } catch (error) {
+      expect(error.message).to.contain(
+        'notNull Violation: product.quantity cannot be null'
+      )
+    }
+  })
+
+  it('quantity cannot be greater than 10000', async () => {
+    try {
+      await Product.create({type: 'Crops', name: 'Crops', quantity: 50000})
+      throw Error('Creating this product should have failed')
+    } catch (error) {
+      expect(error.message).to.contain(
+        'Validation error: Validation max on quantity failed'
+      )
+    }
+  })
+
+  it('quantity cannot be less than 0', async () => {
+    try {
+      await Product.create({type: 'Crops', name: 'Crops', quantity: -1})
+      throw Error('Creating this product should have failed')
+    } catch (error) {
+      expect(error.message).to.contain(
+        'Validation error: Validation min on quantity failed'
+      )
+    }
+  })
+
+  it('price cannot be less than 0', async () => {
+    try {
+      await Product.create({
+        type: 'Crops',
+        name: 'Crops',
+        price: -1,
+        quantity: 10
+      })
+      throw Error('Creating this product should have failed')
+    } catch (error) {
+      expect(error.message).to.contain(
+        'Validation error: Validation min on price failed'
+      )
+    }
+  })
+
+  it('price cannot be null', async () => {
+    try {
+      await Product.create({type: 'Crops', name: 'Crops', quantity: 10})
+      throw Error('Creating this product should have failed')
+    } catch (error) {
+      expect(error.message).to.contain(
+        'notNull Violation: product.price cannot be null'
+      )
+    }
+  })
+
+  it('turns the price into pennies before creation', async () => {
+    const product = await Product.create({
+      type: 'Crops',
+      name: 'Crops',
+      quantity: 10,
+      price: 100
+    })
+
+    expect(product.price).to.be.equal(10000)
+  })
+
+  it('turns the price into pennies before update', async () => {
+    const [numOfAffectedRows, updatedProductPromise] = await Product.update(
+      {price: 600},
+      {
+        returning: true,
+        where: {
+          id: 1
+        }
+      }
+    )
+    expect(numOfAffectedRows).to.deep.equal(1)
+    expect(updatedProductPromise[0].dataValues.price).to.deep.equal(60000)
   })
 })
